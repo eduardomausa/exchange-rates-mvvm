24600ad87e198edf389db8318edf9ca86799fca7
Eduardo Mausa <mausaeduardo@gmail.com>
2023-09-21 21:08:33 -0300

    feat: add the list of currency fluctuations

---
+++ ExchangeRates/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ExchangeRates
+//
+//  Created by Eduardo Mausa on 20/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        RatesFluctuationView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
